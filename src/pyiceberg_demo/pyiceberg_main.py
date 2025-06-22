4b1ab6234965b72ba62e0fbfa7af795ff9ecf389
yzr95924 <yzr95924@gmail.com>
2025-06-23 01:02:34 +0800

    UPDATE: add logging & create table

--- src/pyiceberg_demo/pyiceberg_main.py
+++ src/pyiceberg_demo/pyiceberg_main.py
@@ -1,8 +1,16 @@
 from pyiceberg.catalog import load_catalog
 from pyiceberg.catalog import Catalog
+from pyiceberg.exceptions import NamespaceAlreadyExistsError, TableAlreadyExistsError
 import pyarrow.parquet as pq
+import logging
 
 g_test_parquet_path = "./tmp/test.parquet"
+g_namespace_name = "yzr"
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s: %(message)s",
+    datefmt="%Y/%m/%d-%H:%M:%S"
+)
 
 def connect_to_catalog() -> Catalog:
     warehouse_path = "./tmp"
@@ -18,14 +26,22 @@ def connect_to_catalog() -> Catalog:
 
 if __name__ == "__main__":
     local_catalog = connect_to_catalog()
-    print("connect to catalog done")
+    logging.info("connect to catalog done")
 
     df = pq.read_table(g_test_parquet_path)
 
     # first create namespace
-    local_catalog.create_namespace("default")
-    table = local_catalog.create_table(
-        "default.test_table",
-        schema=pq.read_schema(g_test_parquet_path)
-    )
-    print("create table done")
+    logging.info("create namespace")
+    try:
+        local_catalog.create_namespace(g_namespace_name)
+    except NamespaceAlreadyExistsError as e:
+        logging.warning("namespace already exists")
+
+    logging.info("create table")
+    try:
+        table = local_catalog.create_table(
+            f"{g_namespace_name}.test_table",
+            schema=pq.read_schema(g_test_parquet_path)
+        )
+    except TableAlreadyExistsError as e:
+        logging.warning("table already exists")
